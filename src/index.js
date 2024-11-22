d21459880877b4fc242173b0b8226b8c5610e4e5
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-22 15:39:10 +0530

    first commit Setup And Configuration

---
+++ src/index.js
@@ -0,0 +1,20 @@
+ const express = require('express');
+const bodyParser  = require('body-parser');
+
+ const app = express();
+ const apiRoutes = require('./routes');
+const db = require('./models/index')
+
+
+  const SetupAndStartServer =()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api', apiRoutes);
+    app.listen(3000,()=>{
+        if(process.env.DB_SYNC){
+   db.sequelize.sync({alter:true})
+        }
+        console.log('Server is running Fine NoProblem');
+    })
+  }
+  SetupAndStartServer();
